5847d560886abe8aea49a17d8378249eef17d983
ChrisAlymaras <christos3214@gmail.com>
2026-03-12 16:05:04 +0200

    If this commit apply, every task has a button that the user can click and see the details of the selected task in an expanded container

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ let task4 = {
   notes:"Be careful with the smaller case"
 }
 let task5 = {
-  name: "Football with Friends",
+  name: "Football Match",
   duration: "1h-20min",
   difficulty: 1,
   notes:"Show up 15minutes earlier"
@@ -33,18 +33,65 @@ let tasks = [task1,task2,task3,task4,task5];
 
 //task column initialize
 let column = document.getElementById("to-do");
-//create new elements
+
+//create a div consisting of the task name and a button
 for (let i=0; i<tasks.length; i++){
-  let sp = document.createElement("span");
-  sp.innerHTML = tasks[i].name;
-  sp.classList.add("item");
-  sp.draggable = true;
-  column.appendChild(sp);
+
+  //set task name and classes
+  const task = document.createElement("div");
+  task.innerHTML = tasks[i].name;
+  task.classList.add("item");
+  task.draggable = true;
+
+  //set button for every task
+  const btn = document.createElement("button");
+  btn.classList.add("task-button");
+  btn.innerHTML = "Details";
+
+  //append button to div
+  task.appendChild(btn);
+  //append whole div to-do container
+  column.appendChild(task);
 }
 
-//add listeners for source zone
+const showDetails = document.querySelector(".info-container");
+const allButtons = document.querySelectorAll(".task-button");
+
+for (let i=0; i<allButtons.length; i++) {
+  allButtons[i].addEventListener("click",()=>expandDetails(i));
+}
+
+function expandDetails(i){
+  clearDetails();
+  //set up body for showing task details
+  const header = document.createElement("h1");
+  header.innerHTML = tasks[i].name;
+
+  const duration = document.createElement("h2");
+  duration.innerHTML = `Duration : ${tasks[i].duration}`;
+
+  const difficulty = document.createElement("h3");
+  difficulty.innerHTML = `Difficulty : ${tasks[i].difficulty}`;
+
+  const notes = document.createElement("h4");
+  notes.innerHTML = `Notes : ${tasks[i].notes}`;
+
+  //append to info-container
+  showDetails.appendChild(header);
+  showDetails.appendChild(duration);
+  showDetails.appendChild(difficulty);
+  showDetails.appendChild(notes);
+}
+
+//clear details in order to show another
+function clearDetails(){
+  showDetails.innerHTML = "";
+}
+
+//set up event listeners for drag and drop functionality
 let dragged;
 
+//select items and zones
 document.querySelectorAll(".item").forEach(setEveryElement);
 document.querySelectorAll(".dropzone").forEach(setEveryZone);
 
@@ -52,6 +99,7 @@ function setEveryElement(item) {
   item.addEventListener("drag",onDrag);
 }
 
+//actions triggered when start dragging
 function onDrag(event){
   dragged = event.target;
   event.target.classList.add("dragging");
@@ -59,6 +107,7 @@ function onDrag(event){
   //console.log(dragged);
 }
 
+//actions triggered when stop dragging
 function letDrop(event){
   event.target.addEventListener("dragend", ()=>{
     event.target.classList.remove("dragging");
@@ -66,8 +115,9 @@ function letDrop(event){
   });
 }
 
-//add listeners for every zone
+//add listeners for drop-zones
 
+//prevent default for let dropping
 function setEveryZone(zone){
   zone.addEventListener("dragover",(event)=>{
     event.preventDefault();
@@ -75,6 +125,7 @@ function setEveryZone(zone){
   onDragging(zone);
 }
 
+//actions triggered when entering a zone
 function onDragging(zone){
   zone.addEventListener("dragenter",(event)=>{
     if(event.target.classList.contains("dropzone")){
@@ -84,6 +135,7 @@ function onDragging(zone){
   onLeaving(zone);
 }
 
+//actions when button-up
 function onLeaving(zone){
   zone.addEventListener("dragleave",(event)=>{
     if(event.target.classList.contains("dropzone")){
@@ -93,6 +145,7 @@ function onLeaving(zone){
   addItem(zone);
 }
 
+//actions when dropping
 function addItem(zone){
   zone.addEventListener("drop", (event)=>{
     event.preventDefault();
